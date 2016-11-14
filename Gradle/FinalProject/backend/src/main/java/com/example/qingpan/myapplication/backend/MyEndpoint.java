14fe3627ba8d3c3ba0fb69a5acbd5cbf51cfd3f9
panqing <zzpanqing@gmail.com>
2016-11-14 01:34:08 +0100

    add files

---
+++ Gradle/FinalProject/backend/src/main/java/com/example/qingpan/myapplication/backend/MyEndpoint.java
@@ -0,0 +1,53 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Endpoints Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
+*/
+
+package com.example.qingpan.myapplication.backend;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.javajokes.JavaJokes;
+
+import javax.inject.Named;
+
+/**
+ * An endpoint class we are exposing
+ */
+@Api(
+        name = "myApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "backend.myapplication.qingpan.example.com",
+                ownerName = "backend.myapplication.qingpan.example.com",
+                packagePath = ""
+        )
+)
+public class MyEndpoint {
+
+    /**
+     * A simple endpoint method that takes a name and says Hi back
+     */
+    @ApiMethod(name = "sayHi")
+    public MyBean sayHi(@Named("name") String name) {
+        MyBean response = new MyBean();
+        response.setData("Hi, " + name);
+
+        return response;
+    }
+
+    @ApiMethod(name = "getJavaJokes")
+    public JavaJokes getJavaJokes(){
+        JavaJokes javaJokes = new JavaJokes();
+        return javaJokes;
+    }
+
+    @ApiMethod(name = "getJokeFromMyBean")
+    public MyBean getJokeFromMyBean(){
+        MyBean response = new MyBean();
+        return response;
+    }
+
+}
---
+++ Gradle/FinalProject/libjavajokes/src/main/java/com/javajokes/JavaJokes.java
@@ -0,0 +1,7 @@
+package com.javajokes;
+
+public class JavaJokes {
+    public String getAJoke(){
+        return "JavaJokes tell you a joke.";
+    }
+}
