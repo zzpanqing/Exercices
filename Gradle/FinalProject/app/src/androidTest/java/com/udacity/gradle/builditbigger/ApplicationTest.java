7c1fe40e0c8ad4d54d5eca198afae4578e4a5c31
panqing <zzpanqing@gmail.com>
2016-11-18 01:13:01 +0100

    add an instrument test

--- Gradle/FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
+++ Gradle/FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
@@ -1,13 +1,47 @@
 package com.udacity.gradle.builditbigger;
 
 import android.app.Application;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static org.hamcrest.Matchers.allOf;
+
+
 /**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ * Tests for {@link EndpointsAsyncTask} in  {@link MainActivity}
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ApplicationTest{
+    /**
+     * {@link ActivityTestRule} is a JUnit{@link Rule @Rule} to launch
+     * your activity under test
+     * <p>
+     * Rules are interceptors which are executed for each test method
+     * are important building blocks of Junit tests
+     */
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void clickOnTellAJoke_GetJavaJoke(){
+        onView(withId(R.id.bt_tell_joke))
+                .perform(click());
+        onView(withId(R.id.jokeText))
+                .check(matches(allOf(isDisplayed())));
+
+
     }
 }
