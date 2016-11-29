6ca98c62e102b347f039e9f8d4875d74c598205b
panqing <pan@abvent.fr>
2016-11-29 18:54:13 +0100

    My First Google Map

---
+++ MyFirstMap/app/src/main/java/com/example/qing/myfirstmap/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.qing.myfirstmap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
